e215775abe4d8537ac671c8eec28f41c6e560cdb
swapnasahoo <sahooswapnaswarup@gmail.com>
2026-02-09 11:39:53 +0530

    Add addTodo function

--- main.dart
+++ main.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 class Todo {
   String todoName;
   String todoInfo;
@@ -8,4 +10,24 @@ class Todo {
   void toggleCompleted() {
     isCompleted = !isCompleted;
   }
+}
+
+final List<Todo> todos = [];
+
+void addTodo() {
+  // get todoName
+  stdout.write('Enter your todo name: ');
+  final todoName = stdin.readLineSync() ?? '';
+
+  // get todoInfo
+  stdout.write('Enter todo info: ');
+  final todoInfo = stdin.readLineSync() ?? '';
+
+  // create and store todo
+  final createdTodo = Todo(todoName, todoInfo);
+  todos.add(createdTodo);
+
+  // inform user
+  print('Todo created');
+  print('Todo name: $todoName \nTodo info: $todoInfo \nCompleted: false');
 }
